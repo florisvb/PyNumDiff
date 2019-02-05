40ec8ca6b7328dbba6e2bb338575b2d0318a2802
Floris van Breugel <florisvb@gmail.com>
2019-02-05 10:39:15 -0800

    import neural net methods

--- pynumdiff/__init__.py
+++ pynumdiff/__init__.py
@@ -5,7 +5,8 @@ import pynumdiff.smooth_finite_difference as smooth_finite_difference
 import pynumdiff.total_variation_regularization as total_variation_regularization
 import pynumdiff.linear_model as linear_model
 import pynumdiff.kalman_smooth as kalman_smooth
+import pynumdiff.nnet as nnet
 
 import pynumdiff.utils as utils
-
 import pynumdiff.optimize as optimize
+import pynumdiff.paper as paper

91d9d74f068430bae0c949beacaf0de422409e71
Floris van Breugel <florisvb@gmail.com>
2019-02-05 10:35:00 -0800

    sliding stuff and dmd

--- pynumdiff/linear_model/__linear_model__.py
+++ pynumdiff/linear_model/__linear_model__.py
@@ -8,6 +8,7 @@ import scipy
 # included modules
 from pynumdiff.finite_difference import first_order as finite_difference 
 from pynumdiff.utils import utility as utility
+from pynumdiff import smooth_finite_difference
 __friedrichs_kernel__ = utility.__friedrichs_kernel__
 __gaussian_kernel__ = utility.__gaussian_kernel__
 KERNELS = {'friedrichs': __friedrichs_kernel__,
@@ -201,6 +202,9 @@ def polydiff(x, dt, params, options={'sliding': True, 'step_size': 1, 'kernel_na
 
     if 'sliding' in options.keys() and options['sliding'] is True:
         window_size = copy.copy(params[-1])
+        if window_size < params[0]*3:
+            window_size = params[0]*3+1
+            params[1] = window_size
         return __slide_function__(__polydiff__, x, dt, params, window_size, options['step_size'], options['kernel_name'])
 
     else:
@@ -279,7 +283,9 @@ def chebydiff(x, dt, params, options={'sliding': True, 'step_size': 1, 'kernel_n
 
     if 'sliding' in options.keys() and options['sliding'] is True:
         window_size = copy.copy(params[-1])
-        params = params[0:-1]
+        if window_size < params[0]*2:
+            window_size = params[0]*2+1
+            params[1] = window_size
         return __slide_function__(__chebydiff__, x, dt, params, window_size, options['step_size'], options['kernel_name'])
 
     else:
@@ -315,6 +321,11 @@ def __dmddiff__(X, dt, params, options={}):
     delay_embedding = params[0]
     svd_rank = params[1]
 
+    if len(params) == 2: # not sliding:
+        L = X.shape[0] + delay_embedding
+    else: # sliding
+        L = X.shape[0]
+
     X = X.T
 
     Ue = np.zeros_like(X)
@@ -325,7 +336,7 @@ def __dmddiff__(X, dt, params, options={}):
 
     # DMD reconstruction
     x0 = X[:, 0]
-    Xr = dmdc.forward_backward_reconstruct(x0, X.shape[1], 0, overlap=2)
+    Xr = dmdc.forward_backward_reconstruct(x0, L, 0, overlap=2)
 
     integral_x_hat = np.real(np.ravel( Xr[0,:] ))
     integral_x_hat, x_hat = finite_difference(integral_x_hat, dt)
@@ -360,44 +371,52 @@ def dmddiff(x, dt, params, options={'sliding': True, 'step_size': 10, 'kernel_na
 
     '''
     
+    
+    delay_embedding = params[0]
+    svd_rank = params[1]
+    
+    if delay_embedding >= len(x-1):
+        delay_embedding = len(x-1)
+    if delay_embedding < svd_rank:
+        delay_embedding = svd_rank
+
     if 'sliding' in options.keys() and options['sliding'] is True:
-        delay_embedding = params[0]
-        svd_rank = params[1]
         window_size = copy.copy(params[2])
+        if window_size <= svd_rank + 1:
+            window_size = svd_rank + 1
 
-        # forward
-        integral_x = utility.integrate_dxdt_hat(x, dt)
-        X = utility.hankel_matrix(np.matrix(integral_x), delay_embedding).T
+    # forward
+    integral_x = utility.integrate_dxdt_hat(x, dt)
+    X = utility.hankel_matrix(np.matrix(integral_x), delay_embedding).T
+    if 'sliding' in options.keys() and options['sliding'] is True:
         x_hat_forward, dxdt_hat_forward = __slide_function__(__dmddiff__, X, dt, params, window_size, options['step_size'], options['kernel_name'])
-
-        # backward
-        integral_x = utility.integrate_dxdt_hat(x[::-1], dt)
-        X = utility.hankel_matrix(np.matrix(integral_x), delay_embedding).T
-        x_hat_backward, dxdt_hat_backward = __slide_function__(__dmddiff__, X, dt, params, window_size, options['step_size'], options['kernel_name'])
-
-        # weights
-        w = np.arange(1,len(x_hat_forward)+1,1)[::-1]
-        w = np.pad(w, [0, len(x)-len(w)], mode='constant')
-        wfb = np.vstack((w, w[::-1]))
-        norm = np.sum(wfb, axis=0)
-
-        # orient and pad
-        x_hat_forward = np.pad(x_hat_forward, [0, len(x)-len(x_hat_forward)], mode='constant')
-        x_hat_backward = np.pad(x_hat_backward[::-1], [len(x)-len(x_hat_backward), 0], mode='constant')
-
-        # merge
-        x_hat = x_hat_forward*w/norm + x_hat_backward*w[::-1]/norm
-        x_hat, dxdt_hat = finite_difference(x_hat, dt)
-
-        return x_hat, dxdt_hat
-
     else:
-        delay_embedding = params[0]
-        svd_rank = params[1]
+        x_hat_forward, dxdt_hat_forward = __dmddiff__(X, dt, params, options={})
 
-        integral_x = utility.integrate_dxdt_hat(x, dt)
-        X = utility.hankel_matrix(np.matrix(integral_x), delay_embedding).T
-        return __dmd_smooth__(X, dt, params, options={})
+    # backward
+    integral_x = utility.integrate_dxdt_hat(x[::-1], dt)
+    X = utility.hankel_matrix(np.matrix(integral_x), delay_embedding).T
+    if 'sliding' in options.keys() and options['sliding'] is True:
+        x_hat_backward, dxdt_hat_backward = __slide_function__(__dmddiff__, X, dt, params, window_size, options['step_size'], options['kernel_name'])
+    else:
+        x_hat_backward, dxdt_hat_backward = __dmddiff__(X, dt, params, options={})
+
+    # weights
+    w = np.arange(1,len(x_hat_forward)+1,1)[::-1]
+    w = np.pad(w, [0, len(x)-len(w)], mode='constant')
+    wfb = np.vstack((w, w[::-1]))
+    norm = np.sum(wfb, axis=0)
+
+    # orient and pad
+    x_hat_forward = np.pad(x_hat_forward, [0, len(x)-len(x_hat_forward)], mode='constant')
+    x_hat_backward = np.pad(x_hat_backward[::-1], [len(x)-len(x_hat_backward), 0], mode='constant')
+
+    # merge
+    x_hat = x_hat_forward*w/norm + x_hat_backward*w[::-1]/norm
+    x_hat, dxdt_hat = finite_difference(x_hat, dt)
+
+    return x_hat, dxdt_hat
+
 
 ####################################################################################################################################################
 # Integral formulation
@@ -507,7 +526,7 @@ def lineardiff(x, dt, params, options={'sliding': True, 'step_size': 10, 'kernel
 
     Parameters
     ----------
-    params  : (list)  [N,            : (int)    order of the polynomial
+    params  : (list)  [N,            : (int)    order of the dynamics (e.g. 3 means rank 3 A matrix, with states = {x, xdot, xddot})
                        gamma,        : (float) regularization term
                        window_size], : (int)    size of the sliding window (ignored if not sliding)
     options : (dict) {'sliding'      : (bool)   slide the method (True or False)
@@ -552,7 +571,7 @@ def lineardiff(x, dt, params, options={'sliding': True, 'step_size': 10, 'kernel
 # Spectral derivative
 ####################################################################################################################################################
 
-def spectraldiff(x, dt, params, options={'even_extension': True}): 
+def spectraldiff(x, dt, params, options={'even_extension': True, 'pad_to_zero_dxdt': True}): 
     '''
     Take a derivative in the fourier domain, with high frequency attentuation.
     
@@ -564,9 +583,15 @@ def spectraldiff(x, dt, params, options={'even_extension': True}):
     Parameters
     ----------
     params  : (list) [wn]: (float) the high frequency cut off 
-    options : (dict) {'even_extension'}: (bool) if True, extend the time series with
-                                                an even extension so signal 
-                                                starts and ends at the same value.
+    options : (dict) {'even_extension',:   (bool) if True, extend the time series with
+                                                  an even extension so signal 
+                                                  starts and ends at the same value.
+                      'pad_to_zero_dxdt'}: (bool) if True, extend the time series with
+                                                  extensions that smoothly force the derivative
+                                                  to zero. This allows the spectral derivative
+                                                  to fit data which does not start and end 
+                                                  with derivatives equal to zero.
+
 
     Outputs
     -------
@@ -579,8 +604,21 @@ def spectraldiff(x, dt, params, options={'even_extension': True}):
     else:
         wn = params
 
-    # Do even extension (optional)
     original_L = len(x)
+
+    # make derivative go to zero at ends (optional)
+    if options['pad_to_zero_dxdt']:
+        padding = 100
+        pre = x[0]*np.ones(padding)
+        post = x[-1]*np.ones(padding)
+        x = np.hstack((pre, x, post))
+        x_hat, xdot_hat = smooth_finite_difference.meandiff(x, dt, [int(padding/2)], options={'iterate': False})
+        x_hat[padding:-padding] = x[padding:-padding]
+        x = x_hat
+    else:
+        padding = 0
+
+    # Do even extension (optional)
     if options['even_extension'] is True:
         x = np.hstack((x, x[::-1])) 
 
@@ -602,11 +640,11 @@ def spectraldiff(x, dt, params, options={'even_extension': True}):
 
     # Derivative = 90 deg phase shift
     dxdt_hat = np.real(np.fft.ifft(1.0j * k * np.fft.fft(x)))
-    dxdt_hat = dxdt_hat[0:original_L]
+    dxdt_hat = dxdt_hat[padding:original_L+padding]
 
     # Integrate to get x_hat
     x_hat = utility.integrate_dxdt_hat(dxdt_hat, dt) 
-    x0 = utility.estimate_initial_condition(x[0:original_L], x_hat)
+    x0 = utility.estimate_initial_condition(x[padding:original_L+padding], x_hat)
     x_hat = x_hat + x0
     
     return x_hat, dxdt_hat 

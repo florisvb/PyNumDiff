5903ce5133e1af805feb8467a66be8454e50708d
pavelkomarov <pvlkmrv@gmail.com>
2025-06-25 09:47:22 -0700

    trying to be more specific about what's what so others don't have to dig down in every file like I did

--- linting.py
+++ linting.py
@@ -1,20 +1,13 @@
 # -*- coding: utf-8 -*-
-import re
-import sys
-import argparse
+import re, sys, argparse
 from pylint import lint
 
+# Call `echo $?` to see the exit code after a run. If the score is over this, the exit
+# code will be 0 (success), and if not will be nonzero (failure).
 THRESHOLD = 8.5
 
 if len(sys.argv) < 2:
     raise argparse.ArgumentError("Module to evaluate needs to be the first argument")
 
 sys.argv[1] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[1])
-run = lint.Run([sys.argv[1]], do_exit=False)
-score = run.linter.stats['global_note']
-
-if score < THRESHOLD:
-    print("Your code doesn't pass the PEP8 style score threshold: %f!" % THRESHOLD)
-    sys.exit(1)
-
-print("Congratulations! Your code has passed the PEP8 style score threshold: %f!" % THRESHOLD)
+run = lint.Run([sys.argv[1], f"--fail-under={THRESHOLD}"])
